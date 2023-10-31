69ed1f47dbba95d8bc936445cc4008cd8962088d
bhismapratama <bhismaelki@gmail.com>
2023-10-31 11:01:14 +0700

    feat: kalku vue

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+  data() {
+    return {
+      nomorBang: 0,
+      lanjut: 0,
+      hasil: 0
+    }
+  },
+  methods: {
+    tambah() {
+      this.hasil = this.nomorBang + this.lanjut;
+    },  
+    kurang() {
+      this.hasil = this.nomorBang - this.lanjut;
+    },
+    kali() {
+      this.hasil = this.nomorBang * this.lanjut;
+    },
+    bagi() {
+      this.hasil = this.nomorBang / this.lanjut;
+    },
+  }
+}
+</script>
+
+<template>
+  <main>
+    <header>
+      <h1>kalku vue</h1>
+      <form @submit.prevent="">
+        <label for="nomorBang">nomor awal: </label>
+        <input v-model="nomorBang" type="number" />
+        <br />
+        <label>lanjut: </label>
+        <input v-model="lanjut" type="number" />
+        <br />
+        <label for="operasi">operasi: </label>
+        <button @click="tambah()">tambah</button>
+        <button @click="kurang()">kurang</button>
+        <button @click="kali()">kali</button>
+        <button @click="bagi()">bagi</button>
+        <br/>
+      </form>
+      <p>Hasil perhitungan = {{ hasil }}</p>
+    </header>
+  </main>
+</template>
